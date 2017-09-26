b50acfa07e2f5aa339a8bab50f7b6cde410d0575
virajmanjrekar@gmail.com <virajman@csu.fullerton.edu>
2017-09-25 18:56:02 -0700

    adding different main.js and defining window globaly

--- coffeerun/tests/main1.js
+++ coffeerun/tests/main1.js
@@ -1,4 +1,4 @@
-
+var window = global;
   eval(require('fs').readFileSync('datastore.js', 'utf8'));
   eval(require('fs').readFileSync('truck.js', 'utf8'));
   eval(require('fs').readFileSync('test_datastore.js', 'utf8'));
